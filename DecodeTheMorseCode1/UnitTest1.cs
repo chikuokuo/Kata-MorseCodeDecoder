ca01d65d8bdbd94328d0121446bd56a139215747
Bell Chen Chi Kuo <chikuokuo@msn.com>
2017-12-18 22:42:08 +0800

    version1

--- DecodeTheMorseCode1/UnitTest1.cs
+++ DecodeTheMorseCode1/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace DecodeTheMorseCode1
 {
@@ -7,8 +9,40 @@ namespace DecodeTheMorseCode1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MorseCodeDecoderBasicTest_1()
         {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+    }
+
+    class MorseCodeDecoder
+    {
+        public static string Decode(string morseCode)
+        {
+            var wordSeparate = morseCode.Trim().Split(new[] { "   " }, StringSplitOptions.None);
+            var wordList = wordSeparate.Select(x =>
+                x.Split(' ').Aggregate(string.Empty, (result, y) => result + MorseCode.Get(y)));
+            return string.Join(" ", wordList);
+        }
+    }
+
+    class MorseCode
+    {
+        public static string Get(string p0)
+        {
+            return "0";
         }
     }
 }
